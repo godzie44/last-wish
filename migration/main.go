de4a15921d5bf894a03471cea8786265766a013c
godzie44 <godzie@yandex.ru>
2020-06-13 20:57:36 +0300

    fix migration after change d3 version

--- migration/main.go
+++ migration/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/godzie44/d3/adapter"
+	d3pgx "github.com/godzie44/d3/adapter/pgx"
 	"github.com/godzie44/d3/orm"
 	"github.com/jackc/pgx/v4"
 	"log"
@@ -15,7 +16,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	d3orm := orm.NewOrm(adapter.NewGoPgXAdapter(pg, &adapter.SquirrelAdapter{}))
+	d3orm := orm.NewOrm(d3pgx.NewGoPgXAdapter(pg, &adapter.SquirrelAdapter{}))
 	if err := d3orm.Register(&domain.User{}, &domain.Wish{}); err != nil {
 		log.Fatal(err.Error())
 	}
